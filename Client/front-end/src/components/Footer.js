6c611375e04d1dd25dd390acaae92789621f3e19
tanlucdev <tanluc.dev@gmail.com>
2024-01-15 22:46:27 +0700

    front-end design

---
+++ Client/front-end/src/components/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+    MDBFooter,
+    MDBContainer,
+    MDBCol,
+    MDBRow,
+    MDBIcon,
+    MDBBtn
+} from 'mdb-react-ui-kit';
+
+export default function Footer() {
+    return (
+        <>
+            <div className="footer-container">
+                <MDBFooter className='bg-primary bg-gradient text-center text-white'>
+                    <MDBContainer className='p-4 pb-0'>
+                        <section className='mb-4'>
+                            <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+                                <MDBIcon fab icon='facebook-f' />
+                            </MDBBtn>
+
+                            <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+                                <MDBIcon fab icon='google' />
+                            </MDBBtn>
+                            <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+                                <MDBIcon icon='phone' />
+                            </MDBBtn>
+                        </section>
+                    </MDBContainer>
+
+                    <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+                        © 2024 Copyright: {""}
+                        <a className='text-white' href='https://mdbootstrap.com/'>
+                            YooDict.vn
+                        </a>
+                    </div>
+                </MDBFooter>
+            </div>
+        </>
+    );
+}
---
+++ Client/front-end/src/components/Translate.js
@@ -0,0 +1,31 @@
+const Translate = (props) => {
+    return (<>
+        <div className="translate-container">
+            <main>
+                <div className="translate-area">
+                    <h2> Dịch văn bản</h2>
+                    <form>
+                        <input placeholder="Nhập văn bản cần dịch"></input>
+                        <div>
+                            <button type="submit">Tra từ</button>
+                        </div>
+                    </form>
+                    <div className="translation-result">
+                        <h3>Kết quả dịch</h3>
+                        <p>Đoạn văn dịch thuật</p>
+                    </div>
+                </div>
+                <section className="services-section">
+                    <h2>Dịch vụ của chúng tôi</h2>
+                    <ul>
+                        <li>Dịch thuật tiếng Anh</li>
+                        <li>Dịch thuật tiếng Tây Ban Nha</li>
+                        <li>Dịch thuật tiếng Pháp</li>
+                    </ul>
+                </section>
+            </main>
+        </div>
+    </>)
+}
+
+export default Translate;
