eacb6ed0c0b761a39baadc3de0d8ed028cd0ab03
Pham Thai Hoc <89366106+HocerChicken@users.noreply.github.com>
2024-03-04 21:12:04 +0700

    Update Footer.js

--- Client/src/components/Footer.js
+++ Client/src/components/Footer.js
@@ -1,39 +1,5 @@
 import React from "react";
 import {
-<<<<<<< HEAD
-    MDBFooter,
-    MDBContainer,
-    MDBCol,
-    MDBRow,
-    MDBBtn
-} from 'mdb-react-ui-kit';
-
-export default function App() {
-    return (
-        <div className="footer-container">
-            <MDBFooter className='text-center text-white' style={{ backgroundColor: 'rgb(61, 86, 110)' }}>
-                <MDBContainer className='p-4 pb-0'>
-                    <section className=''>
-                        <p className='d-flex justify-content-center align-items-center'>
-                            <span className='me-3'>Đăng ký miễn phí</span>
-                            <MDBBtn type='button' outline color="light" rounded href='/'>
-                                Đăng ký !
-                            </MDBBtn>
-                        </p>
-                    </section>
-                </MDBContainer>
-
-                <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-                    © 2024 Copyright:
-                    <a className='text-white' href='/'>
-                        YooDict.vn
-                    </a>
-                </div>
-            </MDBFooter>
-        </div>
-    );
-}
-=======
   MDBFooter,
   MDBContainer,
   MDBRow,
@@ -148,4 +114,3 @@ export default function App() {
     </MDBFooter>
   );
 }
->>>>>>> a89f4a4f35d6aceb109c677e56c3d0bbf58fa2cd
