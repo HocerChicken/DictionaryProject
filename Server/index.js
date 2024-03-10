2543543b991ff7fd22facb0150ad9046a635ab49
Pham Thai Hoc <89366106+HocerChicken@users.noreply.github.com>
2024-03-10 20:13:09 +0700

    edit some

--- Server/index.js
+++ Server/index.js
@@ -13,24 +13,8 @@ app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
-
-app.use(express.json());
-
 app.use("/api/words", wordRoutes);
 
-// mongoose.set("strictQuery", false);
-// mongoose
-//     .connect(mongoDBUrl)
-//     .then(() => {
-//         console.log("Successfully connected Database!!!");
-//         app.listen(PORT, () => {
-//             console.log(`App is listening to port: ${PORT}`);
-//         });
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     });
-
-app.listen("5000", () => {
+app.listen(5000, () => {
   console.log("Backend is running.");
 });
