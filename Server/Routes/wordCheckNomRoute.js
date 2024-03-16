708ba1aae8c2dbb1988e143052c4d7858a710dbe
Pham Thai Hoc <89366106+HocerChicken@users.noreply.github.com>
2024-03-17 01:52:53 +0700

    edit some

--- Server/Routes/wordCheckNomRoute.js
+++ Server/Routes/wordCheckNomRoute.js
@@ -19,15 +19,84 @@ router.get("/:quocngu", async (req, res) => {
   }
 });
 
-router.delete("/delete/all", (req, res) => {
-  DictCheckNoms.deleteMany({}, (err) => {
-    if (err) {
-      console.log(err);
-      res.status(500).json(err);
-    } else {
-      res.status(200).json("Delete all successfully");
+// Route to get all words
+router.get("/", async (req, res) => {
+  try {
+    const words = await DictCheckNoms.find();
+
+    if (words.length === 0) {
+      return res.status(404).send({ message: "Not found" });
     }
-  });
+
+    return res.status(200).json(words);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({ message: error.message });
+  }
+});
+
+// Route to get a specific definitioins by "nom"
+router.get("/nom/:nom", async (req, res) => {
+  try {
+    const { nom } = req.params;
+    const words = await DictCheckNoms.find({ "dinhnghia.phanloai.nom": nom });
+
+    const objectArray = [];
+    words.forEach((item) => {
+      const arrayOfObject = item.dinhnghia.phanloai;
+      arrayOfObject.forEach((obj) => {
+        if (obj.nom === nom) {
+          objectArray.push(obj);
+        }
+      });
+    });
+    if (words.length === 0) {
+      return res.status(404).send({ message: "Not found" });
+    }
+    return res.status(200).json(objectArray);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({ message: error.message });
+  }
+});
+
+// Route to get a specific definitioins by "maunicode"
+router.get("/maunicode/:maunicode", async (req, res) => {
+  try {
+    const { maunicode } = req.params;
+    console.log(ma);
+    const words = await DictCheckNoms.find({
+      "dinhnghia.phanloai.maunicode": maunicode,
+    });
+    const objectArray = [];
+    words.forEach((item) => {
+      const arrayOfObject = item.dinhnghia.phanloai;
+      arrayOfObject.forEach((obj) => {
+        console.log(obj.hasOwnProperty("maunicode"));
+        if (obj.maunicode && obj.maunicode === maunicode) {
+          objectArray.push(obj);
+        }
+      });
+    });
+    if (words.length === 0) {
+      return res.status(404).send({ message: "Not found" });
+    }
+    return res.status(200).json(objectArray);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({ message: error.message });
+  }
+});
+
+// Route to delete all words
+router.delete("/", async (req, res) => {
+  try {
+    await DictCheckNoms.deleteMany();
+    return res.status(200).send({ message: "All words deleted" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({ message: error.message });
+  }
 });
 
 module.exports = router;
